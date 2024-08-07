38aedc1f849218647b3b2c39317cd4698fedd850
vickvey <vickvey0303@gmail.com>
2024-08-08 00:00:08 +0530

    added basic homepage functionality

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    name=models.CharField(max_length=256)
+    description=models.TextField()
+    done=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
--- main/views.py
+++ main/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from main import models
 
 # Create your views here.
 def index(request):
-    context={}
+    tasks=models.Task.objects.all()
+    context={
+        'tasks': tasks
+    }
+
     return render(request, 'main/index.html', context)
