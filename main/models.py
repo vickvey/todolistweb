10c93b6680b8cb9242b8ff3128c369db4c9cfe38
vickvey <vickvey0303@gmail.com>
2024-08-08 19:49:41 +0530

    formatted models.py

--- main/models.py
+++ main/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Task(models.Model):
-    name=models.CharField(max_length=256)
-    description=models.TextField()
-    done=models.BooleanField(default=False)
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    done = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
