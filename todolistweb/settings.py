23cbfef9c8823280df19ba1f366e1e01d3ebc787
vickvey <vickvey0303@gmail.com>
2024-08-09 16:27:20 +0530

    chaged the settings.py and ensured security by intro of environ vars

--- todolistweb/settings.py
+++ todolistweb/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9e3^#hal36(v=m8z6)b23@evw7d009+o2dggcbor(=1cdsvs&v'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['vickvey-todolist.netlify.app']
+ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS').split(',')
 
 # Application definition
 
