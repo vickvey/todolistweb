225f799bfc99759045f4c57ccfc69739c326e87c
vickvey <vickvey0303@gmail.com>
2024-08-09 17:50:29 +0530

    changed settings.py

--- todolistweb/settings.py
+++ todolistweb/settings.py
@@ -128,3 +128,12 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
