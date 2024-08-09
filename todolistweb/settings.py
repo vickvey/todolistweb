ca394bab973d487665d8af5b3e218e3732f40b0b
vickvey <vickvey0303@gmail.com>
2024-08-09 18:07:13 +0530

    changed settings.py

--- todolistweb/settings.py
+++ todolistweb/settings.py
@@ -119,11 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
+STATIC_URL = config('STATIC_URL', default='/static/')
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = BASE_DIR / 'staticfiles'
+    STATIC_ROOT = config('STATIC_ROOT', default='staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
